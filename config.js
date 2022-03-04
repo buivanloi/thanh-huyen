8ac4c9462b7770129a14889308a0cb7437a45270
buivanloi <100933683+buivanloi@users.noreply.github.com>
2022-03-04 23:50:50 +0700

    Add files via upload
    huhu

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '8/3';
+const giftUrl = 'https://www.facebook.com/phamthithanhhuyen2003';
+const eventName = 'Bùi Lợi chúc mừng ngày 8-3';
+const titleCard = 'Tặng Dì Huyền';
+const contentCard = 'Chúc Huyền một ngày 8-3 thật là vui vẻ, hạnh phúc và mãi xinh đẹp như chính cái tên của mình vậy. À phải luôn nhớ là có thằng cháu Bùi Lợi ni nữa nghe haha!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'huyen.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
